c3f12f677088e7961851adad5bf29979f8cda252
Muskan-7867 <muskanloach984@gmail.com>
2025-09-03 18:02:37 +0530

    complete about and contact section

---
+++ src/app/(user)/about/page.tsx
@@ -0,0 +1,10 @@
+import { AboutUs } from '@/components/user/aboutus/AboutUs'
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <AboutUs />
+    </div>
+  )
+}
---
+++ src/app/(user)/contact/page.tsx
@@ -0,0 +1,10 @@
+import Contact from '@/components/user/contact/Contact'
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <div className='mt-8 lg:mt-0'>
+      <Contact />
+    </div>
+  )
+}
