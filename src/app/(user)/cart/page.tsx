a9b75a089f50539c50b9830282609519f5fbd17e
Muskan-7867 <muskanloach984@gmail.com>
2025-09-08 18:04:22 +0530

    add cart

---
+++ src/app/(user)/cart/page.tsx
@@ -0,0 +1,13 @@
+import CartLayout from "@/components/user/cart/components/CartLayout"
+
+
+
+const Cart = () => {
+  return (
+    <div>
+      <CartLayout />
+    </div>
+  )
+}
+
+export default Cart
