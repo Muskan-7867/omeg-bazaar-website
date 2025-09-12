cb69902975750d9107b00fc7244242a73cc30c01
Muskan-7867 <muskanloach984@gmail.com>
2025-09-12 13:31:54 +0530

    update

--- src/app/(user)/(home)/page.tsx
+++ src/app/(user)/(home)/page.tsx
@@ -1,29 +1,28 @@
-import HeroSection from '@/components/user/home/HeroSection'
-import HomeBanner from '@/components/user/home/HomeBanner'
-import InfoBar from '@/components/user/home/InfoBar'
-import InstaSection from '@/components/user/home/InstaSection'
-import MusicBanner from '@/components/user/home/MusicBanner'
-import BeautySection from '@/components/user/home/products/BeautyProducts'
-import ProductsSection from '@/components/user/home/products/BeautyProducts'
-import Electronics from '@/components/user/home/products/ElectronicsProducts'
-import KitchenProducts from '@/components/user/home/products/KitchenProducts'
-import React from 'react'
+import HeroSection from "@/components/user/home/HeroSection";
+import HomeBanner from "@/components/user/home/HomeBanner";
+import InfoBar from "@/components/user/home/InfoBar";
+import InstaSection from "@/components/user/home/InstaSection";
+import MusicBanner from "@/components/user/home/MusicBanner";
+import OurApp from "@/components/user/home/OurApp";
+import BeautySection from "@/components/user/home/products/BeautyProducts";
+import Electronics from "@/components/user/home/products/ElectronicsProducts";
+import KitchenProducts from "@/components/user/home/products/KitchenProducts";
+import React from "react";
 
 export default function HomePage() {
   return (
-    <div className='bg-white'>
-       <HomeBanner />
-       <InfoBar />
-       <KitchenProducts />
-       <MusicBanner />
-       <BeautySection/>
-       <HeroSection />
-       <Electronics />
-       {/* <ProductsSection /> */}
-      
-       <InstaSection />
+    <div className="bg-white">
+      <HomeBanner />
+      <InfoBar />
+      <KitchenProducts />
+      <MusicBanner />
+      <Electronics />
+      <HeroSection />
+      <BeautySection />
+      {/* <ProductsSection /> */}
 
-
+      <InstaSection />
+      <OurApp />
     </div>
-  )
+  );
 }
---
+++ src/app/(user)/products/[slug]/page.tsx
@@ -0,0 +1,40 @@
+import SingleProductPage from "@/components/user/products/SingleProductPage";
+import { Product } from "@/lib/types/Product";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+async function getProduct(slug: string): Promise<Product | null> {
+  try {
+    const res = await fetch(`${BASE_URL}/api/v1/product/slug/${slug}`, {
+      cache: "no-store"
+    });
+
+    if (!res.ok) return null;
+
+    const data = await res.json();
+    console.log("from sluf" ,data)
+    return data.product;
+  } catch (error) {
+    console.error("Failed to fetch product:", error);
+    return null;
+  }
+}
+
+export default async function ProductDetailsPage({
+  params
+}: {
+  params: Promise<{ slug: string }> ;
+}) {
+  const { slug} = await params
+  const product = await getProduct(slug);
+
+  if (!product) {
+    return (
+      <div className="p-10 text-red-500 text-center">
+        Product not found or failed to load.
+      </div>
+    );
+  }
+
+  return <SingleProductPage product={product} />;
+}
